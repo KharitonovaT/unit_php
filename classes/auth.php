06841331c26e46d8ccef79464300cb3f82ae4015
KharitonovaT <haritonovatc@gau.local>
2018-11-12 14:11:57 +0300

    function for add

--- classes/auth.php
+++ classes/auth.php
@@ -6,6 +6,22 @@ class Auth
 		array ('login' => 'Tedy2','password' => 'password'));
 
 	public function add( $login, $password ) {
+		$ent=self::viewAll();
+		$exist=false;
+		foreach ($ent as $key => $value) {
+			if($value["login"]==$login){
+				$exist=true;
+			}
+		}
+		if($exist==false){
+			if (strlen($login)<10 && strlen($login)>4 && strlen($password)<10 && strlen($password)>6 ) {
+				if(preg_match("/\w{4,10}/",$login) && !preg_match("/\d{4,10}/",$login)){
+					if(preg_match("/(?=[-_a-zA-Z0-9]*?[A-Z])(?=[-_a-zA-Z0-9]*?[a-z])(?=[-_a-zA-Z0-9]*?[0-9])[-_a-zA-Z0-9]{6,10}/", $password)){
+						self::$_entries[] = array('login' => preg_replace('/\s/', '', $login), 'password' => preg_replace('/\s/', '', $password) );
+					}
+				}
+			}
+		}
 	}
 
 	public function viewAll() {
