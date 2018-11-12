f789d89b631ccebfa668a396415f9a02958bef2c
KharitonovaT <haritonovatc@gau.local>
2018-11-12 12:12:24 +0300

    main working files

---
+++ classes/auth.php
@@ -0,0 +1,9 @@
+<?
+class Auth
+{
+	public function add( $login, $password ) {}
+
+	public function check($login, $password) {}
+
+	public function delete($login, $password) {}
+}
---
+++ test/auth_test.php
@@ -0,0 +1,14 @@
+<?
+require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once('../classes/auth.php');
+
+class TestAuth extends UnitTestCase {
+	function testAuthAdd()
+	{}
+
+	function testAuthCheck()
+	{}
+
+	function testAuthDelete()
+	{}
+}
